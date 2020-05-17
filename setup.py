3990b8386ccc5e4d9b74afb87549626e7de31e2d
takewiki Solutions <hulilei@takewiki.com.cn>
2020-05-17 11:21:38 +0800

    v1.0.1
    baseline

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    reshapedata LLC
+"""
+import platform
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'pyrdt',
+    version = '1.0.1',
+    install_requires=[
+        'requests',
+    ],
+    packages=find_packages(),
+    license = 'Apache License',
+    author = 'Reshapedata',
+    author_email = 'hulilei@takewiki.com.cn',
+    url = 'http://www.reshapedata.com',
+    description = 'reshape data type in py language ',
+    keywords = ['reshapedata', 'rdt','pyrdt'],
+    python_requires='>=3.6',
+)
